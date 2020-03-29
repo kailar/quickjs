3888f3fce6f76a757616cc2620b710bcb3b3528f
kalchas <water.hill@163.com>
2020-03-29 15:47:11 +0800

    WIN32默认输出可执行文件为a.exe

--- qjsc.c
+++ qjsc.c
@@ -617,7 +617,11 @@ int main(int argc, char **argv)
 
     if (!out_filename) {
         if (output_type == OUTPUT_EXECUTABLE) {
+            #if defined(_WIN32)
+            out_filename = "a.exe";
+            #else
             out_filename = "a.out";
+            #endif
         } else {
             out_filename = "out.c";
         }
